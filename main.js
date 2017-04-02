c3f3cc02bca0b6e339f5013e8e2002597bf81cb7
Nick Eddy <nick.a.eddy@gmail.com>
2017-04-01 22:59:06 -0700

    added some more stuff

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var mastermindGame = {
     //array to store the secret code.
     secretCode: [],
     //generates four random numbers between 0 and 7
-    codeGenerator: function() {
+    generateCode: function() {
         var i = 1,j;
         while(i <= 4) {
             j = Math.floor(Math.random() * (8));
@@ -12,18 +12,22 @@ var mastermindGame = {
     },
     //prompts player to guess 4 digits which are then added to an array
     playerGuess: function() {
-        var i = 1;
+        var i = 0;
         var guessArray = [];
-        while(i <= 4) {
-            var guess = prompt("Guess digit number " + i);
-            var guessNum = Number(guess);
-            guessArray.push(guessNum);
-            i++;
+        console.log("Turn number " + this.turnCounter);
+        if(this.turnCounter <= 9) {
+            while(i < 4) {
+                var guess = prompt("Guess digit number " + i);
+                var guessNum = Number(guess);
+                guessArray.push(guessNum);
+                i++;
+            };
+            this.checkResult(guessArray, this.secretCode);
+            this.turnCounter++;
         };
-        this.resultCheck(guessArray, this.secretCode);
     },
     //checks the digits of the guess array against the digits of the code array to see number of correct colors and positions
-    resultCheck: function(guessArray,secretCode) {
+    checkResult: function(guessArray,secretCode) {
         var guessIndex = 0;
         var codeIndex = 0;
         var correctColor = 0;
@@ -38,6 +42,9 @@ var mastermindGame = {
                 correctPosition++;
             };
         };
+        if(correctColor > 4){
+            correctColor = 4;
+        };
         console.log("Number of digits correct: " + correctColor);
         console.log("Number of positions correct: " + correctPosition);
         this.feedbackPegs(correctColor, correctPosition);
@@ -45,11 +52,16 @@ var mastermindGame = {
     //finds number of black and white feedback pegs. Correct position implies correct color.
     feedbackPegs: function(correctColor, correctPosition) {
         var whitePegs = correctColor - correctPosition;
-        console.log("Black pegs: " + correctPosition);
-        console.log("White pegs: " + whitePegs);
-    }
+        this.whitePegs = whitePegs;
+        this.blackPegs = correctPosition;
+        console.log("Black pegs: " + this.blackPegs);
+        console.log("White pegs: " + this.whitePegs);
+    },
+    whitePegs: 0,
+    blackPegs: 0,
+    turnCounter: 8
 };
 
-mastermindGame.codeGenerator();
+mastermindGame.generateCode();
 console.log(mastermindGame.secretCode);
 mastermindGame.playerGuess();
